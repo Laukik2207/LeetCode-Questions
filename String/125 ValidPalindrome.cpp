da34c6ace5aa57b13c5fc029c25ae22418248ec5
Laukik2207 <deshmukhlaukik52@gmail.com>
2026-03-06 23:23:43 +0530

    Added Question

---
+++ String/125 ValidPalindrome.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool isPalindrome(string s){
+    int st = 0;
+    int end = s.length() - 1;
+    while(st < end){
+        while (st < end && !isalnum(s[st])) st++;
+            while (st < end && !isalnum(s[end])) end--;
+
+            if (tolower(s[st]) != tolower(s[end])) {
+                return false;
+            }
+
+            st++;
+            end--;
+
+    }
+    return true;
+}
+
+
+int main(){
+    string s = "Race a Car";
+    if(isPalindrome(s)){
+        cout<<"Palindrome"<<endl;
+    }else{
+        cout<<"Not";
+    }
+}
