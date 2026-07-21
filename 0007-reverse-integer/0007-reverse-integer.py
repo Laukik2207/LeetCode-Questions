935b3401ab894c1eef1fd205ca915156a6b899c2
Laukik Deshmukh <145833962+Laukik2207@users.noreply.github.com>
2026-07-21 12:57:24 +0530

    Time: 46 ms (58%), Space: 19.2 MB (69.59%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -5,7 +5,7 @@ class Solution:
         if(x < 0):
             neg = -1
             x = x*-1
-        x = abs(x)
+        # x = abs(x)
         while x > 0:
             rev = rev*10 + x%10
             x//=10
