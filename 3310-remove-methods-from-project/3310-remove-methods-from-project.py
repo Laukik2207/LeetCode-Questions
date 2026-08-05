682def6fc59938f0090bfe4e32c1bd58eb12ae11
Laukik Deshmukh <145833962+Laukik2207@users.noreply.github.com>
2026-08-05 14:57:40 +0530

    Time: 255 ms (81.06%), Space: 144.5 MB (36.36%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(n)]
+
+        for u, v in invocations:
+            graph[u].append(v)
+
+        # Step 1: Find all suspicious methods
+        suspicious = [False] * n
+
+        def dfs(node):
+            suspicious[node] = True
+            for nei in graph[node]:
+                if not suspicious[nei]:
+                    dfs(nei)
+
+        dfs(k)
+
+        # Step 2: Check if any outside method calls a suspicious method
+        for u, v in invocations:
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
+
+        # Step 3: Return remaining methods
+        ans = []
+        for i in range(n):
+            if not suspicious[i]:
+                ans.append(i)
+
+        return ans
